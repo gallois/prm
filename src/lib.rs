f97780139284ce43b4d540e5c926e6d21f3bc1b6
Felipe Gallois <felipe@gallois.com.br>
2023-03-02 21:43:13 +0000

    Add `add person` prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,50 @@
 use chrono::prelude::*;
 
-struct Person {
+#[derive(Debug)]
+pub struct Person {
     name: String,
-    birthday: DateTime<Utc>,
+    birthday: NaiveDate,
     contact_info: Vec<ContactInfo>,
     activities: Vec<Activity>,
     reminders: Vec<Reminder>,
 }
 
+impl Person {
+    pub fn new(name: String, birthday: NaiveDate, contact_info: Vec<ContactInfo>) -> Person {
+        Person {
+            name,
+            birthday,
+            contact_info,
+            activities: vec![],
+            reminders: vec![],
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Activity {
     activity_type: ActivityType,
     name: String,
-    date: DateTime<Utc>,
+    date: NaiveDate,
     content: String,
 }
 
+#[derive(Debug)]
 enum ActivityType {
     Phone,
     InPerson,
     Online,
 }
 
+#[derive(Debug)]
 struct Reminder {
     name: String,
-    date: DateTime<Utc>,
+    date: NaiveDate,
     recurring: Option<RecurringType>,
     people: Vec<Person>,
 }
 
+#[derive(Debug)]
 enum RecurringType {
     Daily,
     Weekly,
@@ -38,16 +55,19 @@ enum RecurringType {
     Yearly,
 }
 
-struct ContactInfo {
-    contact_info_type: ContactInfoType,
+#[derive(Debug)]
+pub struct ContactInfo {
+    pub contact_info_type: ContactInfoType,
 }
 
-enum ContactInfoType {
+#[derive(Debug)]
+pub enum ContactInfoType {
     Phone(String),
     Whatsapp(String),
     Email(String),
 }
 
+#[derive(Debug)]
 enum Entity {
     Person(Person),
     Activity(Activity),
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::prelude::*;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
@@ -9,7 +10,15 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     #[command(arg_required_else_help = true)]
-    Add { entity: String },
+    Add {
+        entity: String,
+        #[arg(required = false)]
+        name: String,
+        #[arg(required = false)]
+        birthday: String,
+        #[arg(required = false)]
+        contact_info: String,
+    },
     #[command(arg_required_else_help = true)]
     Show { entity: String },
     #[command(arg_required_else_help = true)]
@@ -24,9 +33,44 @@ fn main() {
     let args = Cli::parse();
 
     match args.command {
-        Commands::Add { entity } => {
-            println!("Adding {}", entity);
-        }
+        Commands::Add {
+            entity,
+            name,
+            birthday,
+            contact_info,
+        } => match entity.as_str() {
+            "person" => {
+                let birthday = NaiveDate::parse_from_str(&birthday, "%Y-%m-%d").unwrap();
+                let contact_info_split: Vec<&str> = contact_info.split(":").collect();
+                let contact_info_type;
+
+                match contact_info_split[0] {
+                    "phone" => {
+                        contact_info_type =
+                            prm::ContactInfoType::Phone(String::from(contact_info_split[1]))
+                    }
+                    "whatsapp" => {
+                        contact_info_type =
+                            prm::ContactInfoType::Whatsapp(String::from(contact_info_split[1]))
+                    }
+                    "email" => {
+                        contact_info_type =
+                            prm::ContactInfoType::Email(String::from(contact_info_split[1]))
+                    }
+                    _ => panic!("Unknown contact info type"),
+                }
+
+                let contact_info = prm::ContactInfo {
+                    contact_info_type: contact_info_type,
+                };
+
+                let person = prm::Person::new(name, birthday, vec![contact_info]);
+                println!("Person: {:#?}", person);
+            }
+            other => {
+                println!("Entity unknown: {other}");
+            }
+        },
         Commands::Show { entity } => {
             println!("Showing {}", entity);
         }
